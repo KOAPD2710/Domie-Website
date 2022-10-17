2c7448dffe37a0cab887181ae265aeabcce36f18
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2022-10-18 00:13:40 +0700

    up new

--- js/js_index.js
+++ js/js_index.js
@@ -5,30 +5,5 @@ $(document).ready(function(){
 		var $bgchange = $(this).attr("color-bg");
 		$(".bodygradient").css("background" , $bgchange);
 		/* Change Bg when click Carousel */
-
-
-		$(".carouselitem .charactor .word").removeClass('active');
-		if ($(this).hasClass("bg1")) {
-			$(".carouselitem .charactor .word.bg1").addClass('active');
-		};
-		if ($(this).hasClass("bg2")) {
-			$(".carouselitem .charactor .word.bg2").addClass('active');
-		};
-		if ($(this).hasClass("bg3")) {
-			$(".carouselitem .charactor .word.bg3").addClass('active');
-		};
-		if ($(this).hasClass("bg4")) {
-			$(".carouselitem .charactor .word.bg4").addClass('active');
-		};
-		if ($(this).hasClass("bg5")) {
-			$(".carouselitem .charactor .word.bg5").addClass('active');
-		};
-		if ($(this).hasClass("bg6")) {
-			$(".carouselitem .charactor .word.bg6").addClass('active');
-		};
-		if ($(this).hasClass("bg7")) {
-			$(".carouselitem .charactor .word.bg7").addClass('active');
-		};
 	});
-
 });
