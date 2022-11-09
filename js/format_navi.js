7fbc2908f0db516305c1de45ae9e2c580c3a3eee
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2022-11-09 17:08:33 +0700

    Up fucking new
    hà hà

--- js/format_navi.js
+++ js/format_navi.js
@@ -23,8 +23,10 @@ $(document).ready(function(){
 	$("#navimobile .menu #menucheckbox").click(function(val){
 		if (this.checked == true ) {
 			document.body.style.overflow = 'hidden';
+			$("#menucontainer").addClass("checked");
 		} else {
 			document.body.style.overflow = '';
+			$("#menucontainer").removeClass("checked");
 		}
 	});
 
